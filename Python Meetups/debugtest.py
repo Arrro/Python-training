f98412eb22e92a75e3cdb6f7d6f9f8016c0343de
Arrro <tsteve2012@gmail.com>
2019-10-24 15:53:10 -0700

    Changes to debugtest structure

--- Python Meetups/debugtest.py
+++ Python Meetups/debugtest.py
@@ -3,14 +3,14 @@ import csv
 
 def main():
     with open(r"C:/Users/ts250078/OneDrive - Teradata/Documents/Git-Repos/Python-training/Python-training/Python Meetups/empFile.csv", newline='') as empData:
-        dataRead = csv.DictReader(empData)
+        dataRead = csv.reader(empData)
+        next(dataRead)
+        data = []
         for row in dataRead:
-            fName = row['First Name']
-            lName = row['Last Name']
-            empId = row['EmpId']
+            data.append(row)
     empData.close()
 
-    print(f'Welcome {fName} {lName} youre Employee ID number is {empId}')
+    #print(f'Welcome {fName} {lName} youre Employee ID number is {empId}')
 
 if __name__ == '__main__':
     main()
