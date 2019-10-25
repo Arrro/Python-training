02d57dbd88b1324a32e909185a12528a76d08864
Arrro <tsteve2012@gmail.com>
2019-10-25 14:47:35 -0700

    debugtest FINAL

--- Python Meetups/debugtest.py
+++ Python Meetups/debugtest.py
@@ -1,4 +1,6 @@
 # You can use this if you want to try debugging on your own or to experiment with it more to get a better grasp of how it works!
+# If you run the code below obviously there is some mistakes, while the issue might seem obvious, use some debugging to determine what going where and how its getting there
+
 import csv
 
 def main():
@@ -8,9 +10,8 @@ def main():
         data = []
         for row in dataRead:
             data.append(row)
+            print('Welcome{0}{1} your employee ID is{2}'.format(row[4], row[1], row[3]))
     empData.close()
-
-    #print(f'Welcome {fName} {lName} youre Employee ID number is {empId}')
 
 if __name__ == '__main__':
     main()
