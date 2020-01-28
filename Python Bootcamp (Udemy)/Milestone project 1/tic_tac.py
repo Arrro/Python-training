94f3292711f2d0790a926b2309d56213661e978a
Arrro <tsteve2012@gmail.com>
2020-01-27 22:40:58 -0800

    WOrk continued on milestone1

--- Python Bootcamp (Udemy)/Milestone project 1/tic_tac.py
+++ Python Bootcamp (Udemy)/Milestone project 1/tic_tac.py
@@ -11,12 +11,16 @@ board = [
     1 ,'|', 2 ,'|', 3, '|'
 ]
 
-def win():
-    #win = False
-    win = board[1] and board[2] and board[3] or board[4] and board[5] and board[6] or board[7] and board[8] and board[9]
+
+win = False
+win1 = board[1] and board[2] and board[3] == 'X' or 'O'
+win2 = board[4] and board[5] and board[6] == 'X' or 'O'
+win3 = board[7] and board[8] and board[9] == 'X' or 'O'
+win4 = board[1] and board[5] and board[9] == 'X' or 'O'
+win5 = board[7] and board[5] and board[3] == 'X' or 'O'
 
 #def display_board():
-print(board[1], board[3], board[5], board[6], board[8], board[10], board[12], board[13], board[15], board[17], board[19])
+print('\n', ' ', board[1], board[3], board[5],'\n', '---------', board[6], ' ', board[8], board[10], board[12],'\n','---------', board[13], ' ', board[15], board[17], board[19])
 
 def player_choice():
     pass
@@ -24,8 +28,13 @@ def player_choice():
 def player_select():
     player_1 = ''
     player_2 = ''
-    player_input = input("Player 1 do you wish to be X or O")
+    player_input = input("Player #1 do you wish to be X or O: ")
     if player_input == 'X':
         player_1 == 'X'
     else:
         player_2 == 'O'
+
+while win is False:
+    print('Welcome to Tic Tac Toe!')
+    print('\n', ' ', board[1], board[3], board[5],'\n', '---------', board[6], ' ', board[8], board[10], board[12],'\n','---------', board[13], ' ', board[15], board[17], board[19])
+    player_select()
