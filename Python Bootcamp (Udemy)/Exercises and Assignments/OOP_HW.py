58b688af7f03fa445446465984d59a7601c17d4b
ts250078 <tyler.steve@teradata.com>
2020-02-27 16:20:40 -0800

    OOP HW done

--- Python Bootcamp (Udemy)/Exercises and Assignments/OOP_HW.py
+++ Python Bootcamp (Udemy)/Exercises and Assignments/OOP_HW.py
@@ -4,15 +4,16 @@
 # Fill in the Line class methods to accept coordinates as a pair of tuples and return the slope and distance of the line.
 
 class Line:
-    from math import sqrt
+    import math
     def __init__(self,coor1,coor2):
         self.coor1 = coor1
         self.coor2 = coor2
     def distance(self):
-        d = ((self.coor2[1] - self.coor1[0])**2 + (self.coor2[1] - self.coor1[0])**2)
-        print(self.sqrt(d))
+        d = (self.coor2[0] - self.coor1[0])**2 + (self.coor2[1] - self.coor1[1])**2
+        print(self.math.sqrt(d))
     def slope(self):
-        (self.coor2[1] - self.coor1[1]) / (self.coor2[0] - self.coor1[0])
+        s = (self.coor2[1] - self.coor1[1]) / (self.coor2[0] - self.coor1[0])
+        print(s)
     
 # EXAMPLE OUTPUT
 
@@ -21,24 +22,25 @@ coordinate2 = (8,10)
 
 li = Line(coordinate1,coordinate2)
 li.distance()
-9.433981132056603
+#9.433981132056603
 
 li.slope()
-1.6
+#1.6
 
 # Problem 2
 # Fill in the class
 
 class Cylinder:
-    
-    def __init__(self,height=1,radius=1):
-        pass
-        
+    pi = 3.14
+    def __init__(self,height,radius):
+        self.height = height
+        self.radius = radius
     def volume(self):
-        pass
-    
+        v = self.pi * (self.radius)**2 * self.height
+        print(v)
     def surface_area(self):
-        pass
+        a = 2 * self.pi * self.radius * self.height + 2 * self.pi * (self.radius)**2
+        print(a)
 
 # EXAMPLE OUTPUT
 c = Cylinder(2,3)
