d4f586bd488e06d58a6ceddf2fff2dcab21a6c03
Arrro <tsteve2012@gmail.com>
2020-04-15 18:36:57 -0700

    added proper blackjack logic to hit function

--- Milestone project 2/classes_file.py
+++ Milestone project 2/classes_file.py
@@ -3,26 +3,33 @@ import random
 class Deck:
 
 
-    def __init__(self, cards):
+    def __init__(self, cards, hand, total):
         self.cards = self.cards
+        self.hand = self.hand
+        self.total = self.total
     
     cards = {1:1, 2:2, 3:3, 4:4, 5:5, 6:6, 7:7, 8:8, 9:9, "J":10, "Q":10, "K":10, "A":0}
 
     def player_hand(self):
         hand_count = 0
-        hand = []
+        self.hand = []
         while hand_count < 2:
-            hand.append(random.choice(list(self.cards)))
+            self.hand.append(random.choice(list(self.cards)))
             hand_count += 1
         if hand_count is 2:
-            return hand
+            return self.hand
+
+    
+    def card_total(self):
+        self.total = 0
+        for card in self.hand:
+            self.total += self.cards[card]
+        return self.total
+
 
     def hit(self, hand: list):
-        total = 0
-        for card in hand:
-            if card is "A":
-                card = input("Dealer has dealt an Ace, would like to play it as a 1 or 11?: ")
-                total += int(card)
-            else:
-                total += self.cards[card]
-        return total
+        return self.hand.append(random.choice(self.cards))
+        #for card in self.hand:
+            #if card is "A":
+            #    card = input("You have an ace in your hand, do you wish to play it as a 1 or 11?: ")
+            #    self.total += int(card)
